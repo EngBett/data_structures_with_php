ccba1dd523b0a9971f17a2440525dd1011cb109e
Bett <bettthetimetraveller@gmail.com>
2019-07-30 16:11:22 +0300

    Stack check integer to binary string

---
+++ stacks/integer_to_binary.php
@@ -0,0 +1,27 @@
+<?php
+require_once "Stack.php";
+
+function get_binary($int){
+    $str = "";
+    $stack = new Stack();
+    while ($int!=0){
+        $bin = $int%2;
+        $stack->push((string)$bin);
+        $int = (int)($int/2);
+    }
+
+    while(!$stack->is_empty()){
+        $str .= $stack->pop();
+    }
+
+    return $str;
+}
+
+//test with a
+print_r(get_binary(142));
+
+//new lines
+print_r("\n\n");
+
+//binary string to integer test
+print_r(bindec(get_binary(142)));
